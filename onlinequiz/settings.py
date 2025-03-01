0f95b6789bd054e3c4132d5ed5949bd7504031f4
Yuvraj Singh <whitehat.hacker1108@gmail.com>
2025-03-01 15:53:59 +0530

    update

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -25,7 +25,7 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'static')
 SECRET_KEY = '@k0#p3kidu)yaaa3u1hplxz)f@^6xiy384*(+n@@s5x#1bx@m5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  # Set to True for debugging
+DEBUG = False  # Set to True for debugging
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
